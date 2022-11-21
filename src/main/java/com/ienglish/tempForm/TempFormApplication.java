19cd769e3c9399c930ab7822de18299fbf299684
victor8001 <victor.tsai@tpisoftware.com>
2022-11-21 15:32:41 +0800

    use log4j2 as logger and exclude logback and slf4j dependencies

--- src/main/java/com/ienglish/tempForm/TempFormApplication.java
+++ src/main/java/com/ienglish/tempForm/TempFormApplication.java
@@ -1,5 +1,7 @@
 package com.ienglish.tempForm;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
@@ -12,7 +14,9 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 @EntityScan(basePackages = "com.ienglish.domain")
 public class TempFormApplication {
 
+	private static Logger logger = LogManager.getLogger(TempFormApplication.class);
 	public static void main(String[] args) {
+		logger.info("start web api server ! ");
 		SpringApplication.run(TempFormApplication.class, args);
 	}
 
