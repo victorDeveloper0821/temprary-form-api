f1af201592bc653131bfd94ff5ebf8d2f139c317
victor8001 <victor.tsai@tpisoftware.com>
2022-11-26 09:51:43 +0800

    產生 Token 的工具類

---
+++ src/main/java/com/ienglish/utils/TokenUtils.java
@@ -0,0 +1,8 @@
+package com.ienglish.utils;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public interface TokenUtils {
+    public String generateToken();
+}
---
+++ src/main/java/com/ienglish/utils/impl/GeneralTokenUtils.java
@@ -0,0 +1,10 @@
+package com.ienglish.utils.impl;
+
+import com.ienglish.utils.TokenUtils;
+
+public class GeneralTokenUtils implements TokenUtils {
+    @Override
+    public String generateToken() {
+        return null;
+    }
+}
