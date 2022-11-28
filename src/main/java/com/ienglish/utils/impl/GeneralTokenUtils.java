9008e9012589c18ded53b31661a74988e6b6db54
victor8001 <victor.tsai@tpisoftware.com>
2022-11-28 11:55:30 +0800

    use spring.profiles.active to control properties file

--- src/main/java/com/ienglish/utils/impl/GeneralTokenUtils.java
+++ src/main/java/com/ienglish/utils/impl/GeneralTokenUtils.java
@@ -1,7 +1,9 @@
 package com.ienglish.utils.impl;
 
 import com.ienglish.utils.TokenUtils;
+import org.springframework.stereotype.Component;
 
+@Component
 public class GeneralTokenUtils implements TokenUtils {
     @Override
     public String generateToken() {
